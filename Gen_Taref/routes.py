529a8e1365104e4d9f299bcf6774736427a90ee4
MURILO SERGIO DE SANTANA MACIEL <murilo.ssmaciel@senacsp.edu.br>
2026-06-10 14:59:25 -0300

    Cadastro e Login funcionalidades

--- Gen_Taref/routes.py
+++ Gen_Taref/routes.py
@@ -1,10 +1,17 @@
 from Gen_Taref import app
 from flask import render_template, url_for
 from flask_login import login_required
+from Gen_Taref.forms import FormLogin, FormCriarConta
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/criarconta', methods=['GET', 'POST'])
+def criar_conta():
+    formcriarconta = FormCriarConta()
+    return render_template('criarconta.html')
+
 
 @app.route('/perfil/<usuario>')
 @login_required
